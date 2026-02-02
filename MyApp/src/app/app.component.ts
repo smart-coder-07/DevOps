bb805ad8da9bb1490a536b1ad31eb77b754708ff
atult07 <atul-kumar.a.tiwari@capgemini.com>
2026-02-02 16:01:13 +0530

    routes

--- MyApp/src/app/app.component.ts
+++ MyApp/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = "Kiran";
+
 }
